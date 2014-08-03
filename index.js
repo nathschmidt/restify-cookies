cb51ad6775591e4154f6ad96c2bf2d84a9d01ffa
Nathan Schmidt <nathan.schmidt@abx.com.au>
2014-08-03 17:32:02 +1000

    Refactored to remove some needless complexity

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-module.exports = require("./libs/cookie_parser.js");
+var cookie = require('cookie');
+
+module.exports = {
+	/**
+	 * Parse function to be handed to restify server.use
+	 * 
+	 * @param  {object}   req
+	 * @param  {object}   res 
+	 * @param  {Function} next
+	 * @return {undefined}
+	 */
+	parse: function(req, res, next){
+		var self = this;
+		var cookieHeader = req.headers.cookie;
+
+		if(cookieHeader){
+			req.cookies = cookie.parse(cookieHeader);
+		} else {
+			req.cookies = {};
+		}
+
+		/**
+		 * Add a cookie to our response.  Uses a Set-Cookie header
+		 * per cookie added.
+		 * 
+		 * @param {String} key  - Cookie name
+		 * @param {String} val  - Cookie value
+		 * @param {[type]} opts - Options object can contain path, secure, 
+		 *     expires, domain, http
+		 */
+		res.setCookie = function(key, val, opts){
+
+			var HEADER = "Set-Cookie";
+			if(res.header(HEADER)){
+
+				var curCookies = res.header(HEADER);
+
+				if(!curCookies instanceof Array){
+					curCookies = [curCookies];
+				}
+				
+				curCookies.push( cookies.serialize(key, val, opts) );
+
+				res.header(HEADER, curCookies);
+
+			} else {
+
+				res.header(HEADER, cookie.serialize(key,val, opts));
+
+			}
+		};
+
+		next();
+
+	}
+};
